843f79020ef21bb884ea5d532ff763d5e8c8c833
Tamion <simon.dorner.obf@gmail.com>
2023-05-11 20:00:40 +0200

    Inital Commit2

--- src/main/java/de/tamion/NoConfirm.java
+++ src/main/java/de/tamion/NoConfirm.java
@@ -1,17 +1,19 @@
-package de.noconfirm;
+package de.tamion;
 
+import de.tamion.listeners.ReloadListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoConfirm extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new ReloadListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
