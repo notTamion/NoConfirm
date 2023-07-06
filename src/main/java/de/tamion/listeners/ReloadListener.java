f7a58d471bae3ba22cc970698d7e4d12408c29c2
Tamion <simon.dorner.obf@gmail.com>
2023-07-06 19:06:15 +0200

    Fixed some things + Added bStats

--- src/main/java/de/tamion/listeners/ReloadListener.java
+++ src/main/java/de/tamion/listeners/ReloadListener.java
@@ -1,6 +1,5 @@
 package de.tamion.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -10,17 +9,14 @@ public class ReloadListener implements Listener {
     @EventHandler
     public void onConsole(ServerCommandEvent e) {
         if(e.getCommand().split(" ")[0].equalsIgnoreCase("rl") || e.getCommand().split(" ")[0].equalsIgnoreCase("reload")) {
-            e.setCancelled(true);
-            Bukkit.reload();
-            e.getSender().sendMessage("Reloaded Server");
+            e.setCommand("rl confirm");
         }
     }
     @EventHandler
     public void onMessage(PlayerCommandPreprocessEvent e) {
         if (e.getMessage().split(" ")[0].equalsIgnoreCase("/rl") || e.getMessage().split(" ")[0].equalsIgnoreCase("/reload")) {
             e.setCancelled(true);
-            Bukkit.reload();
-            e.getPlayer().sendMessage("Reloaded Server");
+            e.getPlayer().performCommand("rl confirm");
         }
     }
 }
