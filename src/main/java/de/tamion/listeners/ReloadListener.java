a48076e7fafab5a41d2fbaa6145208857eaa4541
Tamion <70228790+notTamion@users.noreply.github.com>
2023-09-30 10:22:43 +0200

    Deleted unused import

--- src/main/java/de/tamion/listeners/ReloadListener.java
+++ src/main/java/de/tamion/listeners/ReloadListener.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerCommandSendEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 public class ReloadListener implements Listener {
